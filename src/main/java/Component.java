11b188374c5c422305c44e4a8d0d2d465dd3bb85
Jordan Smith <jordsmithy@gmail.com>
2019-10-15 12:09:48 +0100

    first commit

---
+++ src/main/java/Component.java
@@ -0,0 +1,13 @@
+public abstract class Component {
+
+    private String make;
+    private String model;
+
+    public Component(String make, String model) {
+        this.make = make;
+        this.make = model;
+    }
+
+    
+
+}
---
+++ src/main/java/Radio.java
@@ -0,0 +1,9 @@
+public class Radio extends Component {
+
+
+    public String tuneRadio() {
+        return "Beep beep radio tuned";
+    }
+
+
+}
